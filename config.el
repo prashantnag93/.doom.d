6361768df52a0c803c5e1ce075e1698a28a189a9
Prashant Kumar Nag <prashantnag.workmail@gmail.com>
2020-10-18 15:27:45 +0530

    Added Eshell Module and some global settings

--- config.el
+++ config.el
@@ -1,6 +1,12 @@
 (setq user-full-name "Prashant Kumar Nag"
       user-mail-address "prashantnag.workmail@gmail.com")
 
+(setq-default custom-file (expand-file-name ".custom.el" doom-private-dir))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
+(setq avy-all-windows t)
+
 (setq doom-theme 'doom-vibrant)
 
 (setq fancy-splash-image (concat doom-private-dir "banners/banner.png"))
